efea7dacd65299dc719882fd632c6bab69a61a32
Jackson Bailey <jbailey33@leomail.tamuc.edu>
2025-12-05 13:46:19 -0600

    Create route index to register routes

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import {Server} from "@hapi/hapi";
+
+
+/**
+ * Registers application routes to the given server instance.
+ *
+ * @param {Server} server - The server instance to which the routes will be registered.
+ * @return {void} No return value.
+ */
+export function registerRoutes(server: Server): void {
+
+}
