b3554f80f9004f318a3b3ab7450876e91793e572
Jackson Bailey <jbailey33@leomail.tamuc.edu>
2025-12-05 14:33:07 -0600

    Create the barebones eventSchemas to validate the API calls for events

---
+++ src/validators/eventSchemas.ts
@@ -0,0 +1,55 @@
+import Joi from 'joi';
+
+// POST /api/events - Create event
+export const createEventSchema = {
+    payload: Joi.object({
+        title: Joi.string().required().max(255),
+        summary: Joi.string().optional(),
+        contact: Joi.string().email().optional(),
+        url: Joi.string().uri().optional(),
+        start: Joi.date().iso().required(),
+        end: Joi.date().iso().required(),
+        location: Joi.string().optional(),
+        tags: Joi.array().items(Joi.string()).optional()
+    })
+};
+
+// Get /api/events - Get events with query params
+export const listEventsSchema = {
+    query: Joi.object({
+        tag: Joi.string().optional(),
+        audience: Joi.string().optional(),
+        startDate: Joi.date().iso().optional(),
+        endDate: Joi.date().iso().optional()
+    })
+};
+
+// Get /api/events/{id} - Get an event by its ID
+export const getEventByIDSchema = {
+    params: Joi.object({
+        id: Joi.number().integer().positive().required()
+    })
+};
+
+// PUT /api/events/{id} - Update event
+export const updateEventSchema = {
+    params: Joi.object({
+        id: Joi.number().integer().positive().required()
+    }),
+    payload: Joi.object({
+        title: Joi.string().max(255),
+        contact: Joi.string().email().optional(),
+        summary: Joi.string().optional(),
+        url: Joi.string().uri().optional(),
+        start: Joi.date().iso().optional(),
+        end: Joi.date().iso().optional(),
+        location: Joi.string().optional(),
+        tags: Joi.array().items(Joi.string()).optional()
+    })
+};
+
+export const deleteEventSchema = {
+    params: Joi.object({
+        id: Joi.number().integer().positive().required()
+    })
+};
