0d62352a4040c4271346b15e09a99d13825c0b7d
Jackson Bailey <jbailey33@leomail.tamuc.edu>
2025-12-05 18:34:04 -0600

    Create event service for interacting with the database

---
+++ src/services/eventService.ts
@@ -0,0 +1,80 @@
+import {prisma} from "../config/database";
+import {EventFilterParams, CreateEventPayload} from "../models/types";
+
+
+export async function getEvents(filters: EventFilterParams) {
+    const {tag, host, startDate, endDate} = filters;
+
+    return await prisma.event.findMany({
+        where: {
+            ...(tag && {
+                tags: {
+                    some: {
+                        tag: {
+                            name: tag
+                        }
+                    }
+                }
+            }),
+            ...(host && {host: {contains: host}}),
+            ...(startDate && {start: {gte: startDate}}),
+            ...(endDate && {end: {lte: endDate}}),
+        },
+        include: {
+            tags: {
+                include: {
+                    tag: true
+                }
+            },
+        },
+        orderBy: {
+            start: 'asc'
+        }
+    });
+}
+
+export async function  createEvent(data: CreateEventPayload) {
+    const {tags, ...eventData} = data;
+
+    if (eventData.end <= eventData.start) {
+        throw new Error('Event end date must be after start date');
+    }
+
+    return await prisma.event.create({
+        data: {
+            ...eventData,
+            ...(tags && tags.length > 0 && {
+                tags: {
+                    create: tags.map(tagName => ({
+                        tag: {
+                            connectOrCreate: {
+                                where: {name: tagName},
+                                create: {name: tagName}
+                            }
+                        }
+                    }))
+                }
+            })
+        },
+        include: {
+            tags: {
+                include: {
+                    tag: true
+                }
+            }
+        }
+    });
+}
+
+export async function getEventById(id: number) {
+    return await prisma.event.findUnique({
+        where: {id},
+        include: {
+            tags: {
+                include: {
+                    tag: true
+                }
+            }
+        }
+    });
+}
